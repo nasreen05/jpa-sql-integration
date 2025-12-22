63cd83a639aa37e935c77a217234eeac9483f082
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-22 13:44:18 +0530

    laptop table

---
+++ Laptop_Application.sql
@@ -0,0 +1,8 @@
+SELECT * FROM laptop.laptop_info;
+use laptop;
+INSERT INTO laptop_info (company, brand, price, quantity, processor)
+VALUES
+('Inspiron', 'Dell', 65000, 20, 'Intel i5'),
+('Pavilion', 'HP', 72000, 15, 'Ryzen 5'),
+('ThinkPad', 'Lenovo', 80000, 10, 'Intel i7');
+
